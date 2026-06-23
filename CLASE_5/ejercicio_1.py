0b0a02265c454781fd922d3c83edd6fc3ef25fd3
Esteban Trinidad Montoya <estebanmontoya11n@gmail.com>
2026-06-23 17:59:22 -0500

    Delete CLASE_5 directory

--- CLASE_5/ejercicio_1.py
+++
@@ -1,7 +0,0 @@
-a=int(input("INTRODUZCA UN NUMERO AL INICIO DEL CIRCUITO= "))
-b=int(input("INTRODUZCA EL NUMERO DE ALCANZE QUE PUEDE LOGRAR= "))
-c= int(input("DIGA LA SECUENCIA DE DONDE QUIERE IR= "))
-
-
-for a in range(a,b,c):
-    print(a)
