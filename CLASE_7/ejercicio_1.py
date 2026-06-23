6161de1045cdda1fe52889267705ee77539c3345
Esteban Trinidad Montoya <estebanmontoya11n@gmail.com>
2026-06-23 17:59:37 -0500

    Delete CLASE_7 directory

--- CLASE_7/ejercicio_1.py
+++
@@ -1,9 +0,0 @@
-# Programa que almacena cursos en una lista y los muestra en pantalla:
-
-lcursos=["Matemáticas", "Física", "Química", "Historia", "Lengua", "Biología"]
-
-print("lista de cursos")
-print("="*50)
-
-for curso in lcursos:
-    print(curso)
