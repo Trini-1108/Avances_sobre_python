cdfd106fc3cdde2555aebdda8a580cee045b85b6
Esteban Trinidad Montoya <estebanmontoya11n@gmail.com>
2026-06-23 17:59:53 -0500

    Delete Prueba directory

--- Prueba/SEMANAS/SEMANA_2/Ejercicio_3.py
+++
@@ -1,9 +0,0 @@
-#Estructura de control selectiva simple:
-
-a= int(input("Ingrese un dígito:"))
-b= int(input("Ingrese el siguiente dígito:"))
-       
-if b>a: 
-    print("b es mayor que a")
-if b<a:
-    print("b es menor que a")
--- Prueba/SEMANAS/SEMANA_2/Ejercicio_4.py
+++
@@ -1,8 +0,0 @@
-#Estructura de elección doble selectiva:
-a= int(input("Ingrese un numero:"))
-b= int(input("Ingrese un numero:"))
-
-if b>a:
-    print("b es mayor que a")
-else:
-    print("b es menor que a")
